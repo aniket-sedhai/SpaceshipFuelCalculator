28276d052597ec657669a3dcede43b6382ad6f4b
Aniket Sedhai <asedhai@masonite.com>
2024-06-16 12:32:38 -0500

    Initial Commit

---
+++ SpaceshipFuelCalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SpaceshipFuelCalculator
+//
+//  Created by Aniket Sedhai on 6/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
